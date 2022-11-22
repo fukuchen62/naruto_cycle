620f892f1cd2e94ae9ea89a245ef7c6cfe76010a
tetsu <tetsunobu610@gmail.com>
2022-11-22 13:30:53 +0900

    Create header.php
    新規作成
    
    佐藤

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body <?php body_class(); ?>>
+
+    <header>
+        <nav>ヘッダーとナビゲーション
+            <ul>
+                <li>about</li>
+                <li>course</li>
+                <li>search</li>
+                <li>Q&A</li>
+                <li>my page</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+        <?php wp_head(); ?>
+    </header>
