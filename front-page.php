3ac72e8c98a46abd0b7ee09bc04faf189edddbc7
tetsu <tetsunobu610@gmail.com>
2022-11-22 13:31:36 +0900

    Create front-page.php
    新規作成
    
    佐藤

---
+++ front-page.php
@@ -0,0 +1,33 @@
+    <?php get_header(); ?>
+
+
+    <h1>なるとサイくるっと！</h1>
+    <h2><a href=""> ニュース</a> </h2>
+    <h2><a href=""> サイクリングについて</a> </h2>
+    <h2><a href=""> サイクリングコース</a></h2>
+    <h2><a href=""> スポット 検索</a></h2>
+    <h2><a href=""> Ｑ＆Ａ</a></h2>
+    <h2><a href=""> マイページ</a></h2>
+
+    <!-- <footer>
+    <nav>フッターとサイトマップ
+        <ul>
+            <li>about</li>
+            <li>course</li>
+            <li>search</li>
+            <li>Q&A</li>
+            <li>my page</li>
+            <li>contact</li>
+            <li>サイト制作に当たって</li>
+            <li>おといあわせ</li>
+            <li>プライバシーポリシ</li>
+        </ul>
+    </nav>
+</footer>
+
+
+</body>
+
+</html> -->
+
+    <?php get_footer(); ?>
