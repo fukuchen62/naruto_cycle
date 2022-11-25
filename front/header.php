8a7592dff4013e06f4a279ebd68015a4773ed2d3
takase <takanaoseko@gmail.com>
2022-11-25 13:40:16 +0900

    header,footerを編集しました
    header,footer.phpを編集しました
    
    髙瀬

--- front/header.php
+++ front/header.php
@@ -1,1 +1,40 @@
-test
+<!-- ヘッダー切り取りここから -->
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="鳴門を自転車でぐるっと巡るサイクリングコースを紹介。定番の観光地から穴場スポットまで網羅したおススメコースをマップで確認！知っておきたい注意点や駐輪場の事まで詳しく掲載。鳴門の自転車旅には欠かせないサイクリング情報サイトです。" />
+    <meta name="keywords" content="サイクリングマップ,四国,サイクルショップ,ASA,淡路島,大鳴門橋,おすすめ,徳島,鳴門,観光,サイクリング,自転車、絶景,サイクリングコース" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="format-detection" content="telephone=no" />
+    <title>なるとサイくるっと！</title>
+    <!-- 外部css -->
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/footer.css" />
+    <link rel="stylesheet" href="css/index.css" />
+    <link rel="stylesheet" href="css/wave.scss">
+    <!-- googlefont本体ここから2行 -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <!-- 全体用font↓ -->
+
+    <!-- h1のfont↓ -->
+
+    <!-- FontAwesome↓ -->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
+    <!-- jQuery本体↓ -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <!-- ファビコン↓() -->
+</head>
+
+<body>
+
+    <header class="header">
+        <h1 class="header_logo"><a href="#">ロゴが入ります<img src="" alt="なるとサイくるっと！のロゴ"></a></h1>
+        <nav class="gnav"></nav>
+    </header>
+    <!-- ヘッダー切り取りここまで -->
