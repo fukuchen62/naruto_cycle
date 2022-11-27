579eba348303a16a90d0827d5608559114bf8dbf
Mi-Kondo <km1tsurug1126@gmail.com>
2022-11-27 11:35:50 +0900

    コース詳細更新
    コース詳細を更新しました。
    
    近藤

--- front/js/course.js
+++ front/js/course.js
@@ -1,8 +1,8 @@
 "use strict";
 
 $(".course_nav").click(function () {
-	$(".iframe_wrapper").removeClass("short");
-	$(".iframe_wrapper").removeClass("middle");
-	$(".iframe_wrapper").removeClass("long");
+	// $(".iframe_wrapper").removeClass("short");
+	// $(".iframe_wrapper").removeClass("middle");
+	// $(".iframe_wrapper").removeClass("long");
 	$(".iframe_wrapper").addClass("beginner");
 });
