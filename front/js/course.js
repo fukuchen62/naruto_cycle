955821dd79447cfd79606e69bad094f5a5829bdc
Mi-Kondo <km1tsurug1126@gmail.com>
2022-11-27 18:06:02 +0900

    コース詳細更新
    コース詳細を更新しました。
    
    近藤

--- front/js/course.js
+++ front/js/course.js
@@ -1,8 +1,26 @@
 "use strict";
 
-$(".course_nav").click(function () {
-	// $(".iframe_wrapper").removeClass("short");
-	// $(".iframe_wrapper").removeClass("middle");
-	// $(".iframe_wrapper").removeClass("long");
+$(".course_nav.beginner").click(function () {
+	$(".iframe_wrapper").removeClass("short");
+	$(".iframe_wrapper").removeClass("middle");
+	$(".iframe_wrapper").removeClass("long");
 	$(".iframe_wrapper").addClass("beginner");
+});
+$(".course_nav.short").click(function () {
+	$(".iframe_wrapper").removeClass("begginer");
+	$(".iframe_wrapper").removeClass("middle");
+	$(".iframe_wrapper").removeClass("long");
+	$(".iframe_wrapper").addClass("short");
+});
+$(".course_nav.middle").click(function () {
+	$(".iframe_wrapper").removeClass("begginer");
+	$(".iframe_wrapper").removeClass("short");
+	$(".iframe_wrapper").removeClass("long");
+	$(".iframe_wrapper").addClass("middle");
+});
+$(".course_nav.long").click(function () {
+	$(".iframe_wrapper").removeClass("begginer");
+	$(".iframe_wrapper").removeClass("short");
+	$(".iframe_wrapper").removeClass("middle");
+	$(".iframe_wrapper").addClass("long");
 });
