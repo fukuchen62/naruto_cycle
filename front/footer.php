e6c1139fccb568356934138236ab9a4c6be0ad3a
takase <takanaoseko@gmail.com>
2022-11-29 13:29:17 +0900

    header,footer更新
    header,footer更新
    
    髙瀬

--- front/footer.php
+++ front/footer.php
@@ -1,7 +1,7 @@
 <!-- フッター切り取りここから -->
 
 <footer class="footer bg_blue white txt_c debug">
-    <ul class="banner_list gap32_16">
+    <ul class="banner_list gap32_16 m_bottom40">
         <!-- asaなどのバナーがliで入ります -->
         <li class="banner_item"><a href=""><img src="https://placehold.jp/234x60.png" alt="○○のバナー"></a></li>
         <li class="banner_item"><a href=""><img src="https://placehold.jp/234x60.png" alt="○○のバナー"></a></li>
@@ -10,104 +10,54 @@
     <nav class="fnav_container bg_blue debug">
 
         <!-- フッターナビ -->
+        <!-- 近藤追加分 -->
         <ul class="fnav">
-
-            <!-- フッターナビメニューの見出し -->
-            <!-- リストがないものはこのliのみ -->
-            <li class="fnav_menu">
-                <!-- フッターナビメニューのリスト -->
-                <ul class="fnav_list between gap32_16 ">
-                    <!-- リストの内容 -->
-                    <!-- フッターNEWS/HOWTO/MYPAGE -->
-                    <li class="fnav_menu">
-                        <!-- フッターナビメニューのリスト -->
-                        <ul class="fnav_list">
-                            <!-- リストの内容 -->
-                            <li class="fnav_item n_h_m">
-                                <a href="news.html">ニュース</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href="howto.html">鳴門サイクリングについて</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href="mypage.html">マイページ</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- フッターCOURSE -->
-                    <li class="fnav_menu">
-                        サイクリングコース
-                        <!-- フッターナビメニューのリスト -->
-                        <ul class="fnav_list">
-                            <!-- リストの内容 -->
-                            <li class="fnav_item">
-                                <a href=".html">初心者モデルコース</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">短距離コース</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">中距離コース</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">長距離コース</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- フッターSPOT -->
-                    <li class="fnav_menu">
-                        周辺スポット検索
-                        <!-- フッターナビメニューのリスト -->
-                        <ul class="fnav_list">
-                            <!-- リストの内容 -->
-                            <li class="fnav_item">
-                                <a href=".html">食べる</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">見る</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">買う</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">楽しむ</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href=".html">サイクルスポット</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="fnav_menu">
-                        Q＆A・その他
-                        <!-- フッターナビメニューのリスト -->
-                        <ul class="fnav_list">
-                            <!-- リストの内容 -->
-                            <li class="fnav_menu">
-                                <a href="question.html">Q&A</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href="contact.html">お問い合わせ</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href="privacy.html">プライバシーポリシー・免責事項</a>
-                            </li>
-                            <li class="fnav_item">
-                                <a href="aboutsite.html">サイト制作にあたって</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+            <ul class="fnav_w_list flex column">
+                <li class="footer_headline">
+                    <ul class="footer_list">
+                        <li class="footer_nav_item bold"><a href="news.html">ニュース</a></li>
+                        <li class="footer_nav_item bold"><a href="howto.html">鳴門サイクリングについて</a></li>
+                        <li class="footer_nav_item bold"><a href="mypage.html">マイページ</a></li>
+                    </ul>
+                </li>
+                <li class="footer_headline"><a href="course.html">サイクリングコース</a>
+                    <ul class="footer_list">
+                        <li class="footer_nav_item"><a href=".html"></a>初心者モデルコース</li>
+                        <li class="footer_nav_item"><a href=".html">短距離コース</a></li>
+                        <li class="footer_nav_item"><a href=".html">中距離コース</a></li>
+                        <li class="footer_nav_item"><a href=".html">長距離コース</a></li>
+                    </ul>
+                </li>
+                <li class="footer_headline"><a href=".html">周辺スポット検索</a>
+                    <ul class="footer_list">
+                        <li class="footer_nav_item"><a href=".html">食べる</a></li>
+                        <li class="footer_nav_item"><a href=".html">見る</a></li>
+                        <li class="footer_nav_item"><a href=".html">買う</a></li>
+                        <li class="footer_nav_item"><a href=".html">楽しむ</a></li>
+                        <li class="footer_nav_item"><a href=".html">サイクルスポット</a></li>
+                    </ul>
+                </li>
+                <li class="footer_headline">Q＆A・その他
+                    <ul class="footer_list">
+                        <li class="footer_nav_item"><a href="question.html">Q＆A</a></li>
+                        <li class="footer_nav_item"><a href="contact.html">お問い合わせ</a></li>
+                        <li class="footer_nav_item"><a href="privacy.html">プライバシーポリシー・免責事項</a></li>
+                        <li class="footer_nav_item"><a href="aboutsite.html">サイト制作にあたって</a></li>
+                    </ul>
+                </li>
+            </ul>
     </nav>
+    <!-- 追加分ここまで -->
+
     <div class="sns_container flex gap32_16">
         <!-- twitter -->
         <a href=""><img class="sns_icon" src="./image/Twitter social icons - circle - blue.png" alt="ツイッターアイコン"></a>
         <!-- facebook -->
         <a href=""><img class="sns_icon" src="./image/f_logo_RGB-Blue_144.png" alt="ツイッターアイコン"></a>
     </div>
-    <p class="copyright">AllCopyRight©なるとサイくるっと！</p>
+    <p class="copyright txt_c">AllCopyRight©なるとサイくるっと！</p>
 </footer>
-<script src="./js/common.js"></script>
+<script src="./js/index.js"></script>
 <script src="js/jquery.bgswitcher.js"></script>
 <script src="js/bgswitcher.js"></script>
 </div>
