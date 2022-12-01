c220613dc3c0e88c39f877e79435e184ef7386be
tetsu <tetsunobu610@gmail.com>
2022-12-01 15:10:02 +0900

    更新
    更新
    
    佐藤

--- footer.php
+++ footer.php
@@ -1,34 +1,69 @@
-<footer class="footer">
-    <div class="container">
-        <div class="footer_inner">
+<!--＝＝＝＝＝＝＝＝＝＝＝＝＝ footerここから ＝＝＝＝＝＝＝＝＝＝＝＝＝-->
 
-            <nav>フッターとサイトマップ
-                <ul>
-                    <?php
-                    $args = array(
-                        'menu' => 'footer-navigation', //作成したメニュー名
-                        'menu_class' => '', //メニューに使うクラス名
-                        'container' => false, //ulタグを囲んでいるdivを削除
-                    );
-                    wp_nav_menu($args);
-                    ?>
+<footer class="footer bg_blue white txt_c">
+    <ul class="banner_list gap32_16 m_bottom40">
+        <!-- asaなどのバナーがliで入ります -->
+        <li class="banner_item"><a href=""><img src="https://placehold.jp/234x60.png" alt="○○のバナー"></a></li>
+        <li class="banner_item"><a href=""><img src="https://placehold.jp/234x60.png" alt="○○のバナー"></a></li>
+        <li class="banner_item"><a href=""><img src="https://placehold.jp/234x60.png" alt="○○のバナー"></a></li>
+    </ul>
 
-                    <!-- <li>about</li>
-            <li>course</li>
-            <li>search</li>
-            <li>Q&A</li>
-            <li>my page</li>
-            <li>contact</li>
-            <li>サイト制作に当たって</li>
-            <li>おといあわせ</li>
-            <li>プライバシーポリシ</li> -->
+    <nav class="fnav_container bg_blue">
+        <!-- フッターナビ -->
+        <!-- 近藤追加分 -->
+        <ul class="fnav_w_list">
+            <li class="footer_headline">
+                <ul class="footer_list">
+                    <li class="footer_nav_item bold"><a href="news.html">ニュース</a></li>
+                    <li class="footer_nav_item bold"><a href="howto.html">鳴門サイクリングについて</a></li>
+                    <li class="footer_nav_item bold"><a href="mypage.html">マイページ</a></li>
                 </ul>
-            </nav>
-        </div>
+            </li>
+            <li class="footer_headline"><a href="course.html">サイクリングコース</a>
+                <ul class="footer_list">
+                    <li class="footer_nav_item"><a href=".html"></a>初心者モデルコース</li>
+                    <li class="footer_nav_item"><a href=".html">短距離コース</a></li>
+                    <li class="footer_nav_item"><a href=".html">中距離コース</a></li>
+                    <li class="footer_nav_item"><a href=".html">長距離コース</a></li>
+                </ul>
+            </li>
+            <li class="footer_headline"><a href=".html">周辺スポット検索</a>
+                <ul class="footer_list">
+                    <li class="footer_nav_item"><a href=".html">食べる</a></li>
+                    <li class="footer_nav_item"><a href=".html">見る</a></li>
+                    <li class="footer_nav_item"><a href=".html">買う</a></li>
+                    <li class="footer_nav_item"><a href=".html">楽しむ</a></li>
+                    <li class="footer_nav_item"><a href=".html">サイクルスポット</a></li>
+                </ul>
+            </li>
+            <li class="footer_headline">Q＆A・その他
+                <ul class="footer_list">
+                    <li class="footer_nav_item"><a href="question.html">Q＆A</a></li>
+                    <li class="footer_nav_item"><a href="contact.html">お問い合わせ</a></li>
+                    <li class="footer_nav_item"><a href="privacy.html">プライバシーポリシー・免責事項</a></li>
+                    <li class="footer_nav_item"><a href="aboutsite.html">サイト制作にあたって</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <!-- 追加分ここまで -->
+
+    <div class="sns_container flex gap32_16">
+        <!-- twitter -->
+        <a href=""><img class="sns_icon" src="<?php echo get_template_directory_uri(); ?>//assets/img/index_footer_sharetwitter.png" alt=" Twitterへのシェアボタン"></a>
+        <!-- facebook -->
+        <a href=""><img class="sns_icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/index_footer_sharefb.png" alt="Facebookへのシェアボタン"></a>
     </div>
+    <p class="copyright txt_c">AllCopyRight©なるとサイくるっと！</p>
 </footer>
 
+<!--＝＝＝＝＝＝＝＝＝＝＝＝＝ footerここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/header.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
+</div>
 <?php wp_footer(); ?>
 </body>
 
 </html>
+<!-- フッター切り取りここまで -->
--- header.php
+++ header.php
@@ -2,36 +2,78 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.min.css" rel="stylesheet">
+    <meta charset="UTF-8" />
+    <meta name="description" content="鳴門を自転車でぐるっと巡るサイクリングコースを紹介。定番の観光地から穴場スポットまで網羅したおススメコースをマップで確認！知っておきたい注意点や駐輪場の事まで詳しく掲載。鳴門の自転車旅には欠かせないサイクリング情報サイトです。" />
+    <meta name="keywords" content="サイクリングマップ,四国,サイクルショップ,ASA,淡路島,大鳴門橋,おすすめ,徳島,鳴門,観光,サイクリング,自転車、絶景,サイクリングコース" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="format-detection" content="telephone=no" />
+    <!-- <title>なるとサイくるっと！</title> -->
+    <!-- 外部css -->
+    <!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/common.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/header.css" /> -->
+    <!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/footer.css" /> -->
+    <!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/index.css" /> -->
+
+    <!--↓今は使っていないが今後別バージョンで読み込む  -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/wave.css" />
+
+    <!-- googlefont本体ここから2行 -->
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> -->
+    <!-- 全体用font↓ -->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Noto+Sans+JP:wght@400;900&display=swap" rel="stylesheet" /> -->
+    <!-- h1のfont↓ -->
+
+    <!-- FontAwesome↓ -->
+    <!-- <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" /> -->
+    <!-- jQuery本体↓ -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
+    <!-- ファビコン↓() -->
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-
-    <?php wp_body_open(); ?>
-    <header class="header">
-        <div class="header_links">
-            <nav class="gnav">ヘッダーとナビゲーション
-                <ul>
-                    <?php
-                    $args = array(
-                        'menu' => 'global-navigation', //作成したメニュー名
-                        'menu_class' => '', //メニューに使うクラス名
-                        'container' => false, //ulタグを囲んでいるdivを削除
-                    );
-                    wp_nav_menu($args);
-                    ?>
-
-                    <!-- <li>about</li>
-                <li>course</li>
-                <li>search</li>
-                <li>Q&A</li>
-                <li>my page</li>
-                <li>contact</li> -->
+    <div class="wrap">
+        <!--＝＝＝＝＝＝＝＝＝＝＝＝＝ headerここから ＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+        <header class="header flex">
+            <div class="header_logo">
+                <h1>
+                    <a href="<?php echo home_url(); ?>">
+                        <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/header_logo_img.png" alt="なるとサイくるっと！のロゴ" />
+                    </a>
+                </h1>
+            </div>
+            <nav>
+                <ul class="menu">
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo home_url('/news/'); ?>">NEWS</a>
+                    </li>
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo get_permalink(2); ?>">HOWTO</a>
+                    </li>
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo get_permalink(183); ?>">COURSE</a>
+                    </li>
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo home_url('/spot/'); ?>">SPOT</a>
+                    </li>
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo home_url('/mypage/'); ?>">MYPAGE</a>
+                    </li>
+                    <li class="menu-list font_edge_white">
+                        <a href="<?php echo home_url('/question/'); ?>">Q&A</a>
+                    </li>
                 </ul>
+                <button type="button" class="btn js-btn">
+                    <span class="btn-line"></span>
+                </button>
             </nav>
-            <?php wp_head(); ?>
-        </div>
-    </header>
+        </header>
+        <!--＝＝＝＝＝＝＝＝＝＝＝＝＝ headerここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+    </div>
+</body>
+
+</html>
