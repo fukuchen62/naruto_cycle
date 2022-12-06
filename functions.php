7440c3f2ac80bc5251fcb7e9ff667015ac3e359e
Mi-Kondo <km1tsurug1126@gmail.com>
2022-12-06 11:29:48 +0900

    functionsとhowto更新
    functinosにhowtoのslickを追加しました。

--- functions.php
+++ functions.php
@@ -245,9 +245,19 @@ function add_individual_stylesheet_script()
     // 02-C:howtoページ
     else if (is_page('cycle')) {
         wp_enqueue_style('naruto_cycle-howto', get_template_directory_uri() . '/assets/css/howto.css', array(), false);
+        //slickCSSを読み込む
+        wp_enqueue_style('slick', get_template_directory_uri() . '/assets/slick/slick.css', false);
+
+        //slick-themeCSSを読み込む
+        wp_enqueue_style('slick-theme', get_template_directory_uri() . '/assets/slick/slick-theme.css', false);
+
         // js
         wp_enqueue_script('naruto_cycle-howto-script', get_template_directory_uri() . '/assets/js/howto.js', '', '', true);
+
+        //slickのJSを読み込み
+        wp_enqueue_script('slick-min', get_template_directory_uri() . '/assets/slick/slick.min.js', '', '', true);
     }
+
     // 02-D:courseページ
     else if (is_singular('course')) {
         wp_enqueue_style('naruto_cycle-course', get_template_directory_uri() . '/assets/css/course.css', array(), false);
