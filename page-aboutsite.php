301422dfac6acdb01567fe401caa4c2d6147d9f1
Mi-Kondo <km1tsurug1126@gmail.com>
2022-12-06 15:14:21 +0900

    aboutsite更新

--- page-aboutsite.php
+++ page-aboutsite.php
@@ -1,4 +1,129 @@
 <?php get_header(); ?>
-<h2>サイト制作に当たって</h2>
+
+<main>
+    <!-- ページ見出し -->
+    <section class="aboutsite">
+        <h2 class="course_top headline"><ruby>ABOUT SITE<rt>このサイトについて</rt></ruby></h2>
+    </section>
+    <section class="about_container">
+        <!-- コンテンツボックス -->
+        <div class="about_box flex_pc">
+            <!-- 最初のキャプション -->
+            <p class="about_txt">このサイトは、国の求職者支援制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
+            </p>
+            <!-- 写真 -->
+            <div class="index_howto_imgbox">
+                <img src=src="<?php echo get_template_directory_uri(); ?>/assets\img\index_howto_img.png" alt="浜辺に自転車を置いて海に向かう男性" class="index_howto_img">
+            </div>
+            <!-- テキスト用ボックス -->
+            <div class="about_txtbox relative">
+
+                <!-- 見出し文 -->
+                <h3 class="about_h3">なぜこのサイトを作ったのか</h3>
+                <!-- キャプション -->
+                <p class="about_txt">
+                    このサイトは、国の求職者支援制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
+                </p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="about_container">
+        <!-- コンテンツボックス -->
+        <div class="about_box flex_pc">
+            <!-- テキスト用ボックス -->
+            <div class="about_txtbox relative">
+                <!-- 見出し文 -->
+                <h3 class="about_h3">サイト制作に<br>
+                    ご協力くださったみなさま</h3>
+                <!-- キャプション -->
+                <p class="about_txt ">この度は本サイト制作に際して、ご協力いただき誠にありがとうございます。</p>
+                <!-- ショップ一覧 -->
+                <div class="about_thanks_wrap">
+                    <table class="about_thanks_table">
+                        <tbody>
+                            <tr>
+                                <td>ベーカリーカフェ様</td>
+
+                                <td>ベーカリーカフェ様</td>
+                            </tr>
+
+                            <tr>
+                                <td>ベーカリーカフェ様</td>
+
+                                <td>ベーカリーカフェ様</td>
+                            </tr>
+
+                            <tr>
+                                <td>カフェAAA様</td>
+
+                                <td>その他大勢の飲食店様</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <p>その他ご協力くださったみなさま</p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="about_container">
+        <!-- コンテンツボックス -->
+        <div class="about_box flex_pc">
+            <!-- テキスト用ボックス -->
+            <div class="about_txtbox relative">
+                <!-- 見出し文 -->
+                <h3 class="about_h3">参考サイト</h3>
+                <!-- キャプション -->
+                <p class="index_howto_txt ">
+                    以下のサイトを参考元・引用元として、まとめさせて頂きました。 各関係機関にご協力いただき、リンク先を掲載させていただいております。 アドレスをクリックすると、リンク先へ飛びます。
+                </p>
+                <ul>
+                    <li>
+                        ベーカリーショップ
+                    </li>
+                    <li>
+                        ベーカリーショップ
+                    </li>
+                    <li>
+                        ベーカリーショップ
+                    </li>
+                    <li>
+                        ベーカリーショップ
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                    <li>
+                        カフェAAA
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </section>
+</main>
+
 
 <?php get_footer(); ?>
