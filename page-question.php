7644599b2510ebe75a53bc794f73abd41df3297a
tetsu <tetsunobu610@gmail.com>
2022-12-06 15:30:50 +0900

    Q&Aキービジュアル追加
    キービジュ追加
    
    佐藤

--- page-question.php
+++ page-question.php
@@ -3,7 +3,7 @@
 <main>
     <!-- キービジュアル -->
     <div class="key_bg_under">
-        <img src="https://placehold.jp/1440x400.png" alt="マイページキービジュアル" class="key_bg_under_img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/key_visual_lightblue.png" alt="マイページキービジュアル" class="key_bg_under_img">
     </div>
 
     <!-- パンくずリスト -->
