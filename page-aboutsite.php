eadb6c4c7b9d423b9cff1eb527005eceac8b5e75
bando412 <sys5rb639@gmail.com>
2022-12-08 18:51:23 +0900

    諸々更新（坂東）

--- page-aboutsite.php
+++ page-aboutsite.php
@@ -1,22 +1,26 @@
 <?php get_header(); ?>
-
 <main>
+    <!-- キービジュアル -->
+    <div class="key_bg_under">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/key_visual_lightblue.png" alt="マイページキービジュアル" class="key_bg_under_img">
+    </div>
     <!-- ページ見出し -->
     <section class="aboutsite">
         <h2 class="course_top headline"><ruby>ABOUT SITE<rt>このサイトについて</rt></ruby></h2>
     </section>
     <section class="about_container">
         <!-- コンテンツボックス -->
-        <div class="about_box flex_pc">
+        <div class="about_box relative">
             <!-- 最初のキャプション -->
-            <p class="about_txt">このサイトは、国の求職者支援制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
+            <p class="about_txt">このサイトは、国の公的職業訓練制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
             </p>
-            <!-- 写真 -->
-            <div class="index_howto_imgbox">
-                <img src=src="<?php echo get_template_directory_uri(); ?>/assets\img\index_howto_img.png" alt="浜辺に自転車を置いて海に向かう男性" class="index_howto_img">
+            <div class="flex_pc">
+                <!-- 写真 -->
+                <!-- <div class="index_howto_imgbox"> -->
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index_howto_img.png" alt="浜辺に自転車を置いて海に向かう男性" class="index_howto_img">
             </div>
             <!-- テキスト用ボックス -->
-            <div class="about_txtbox relative">
+            <div class="about_txtbox absolute">
 
                 <!-- 見出し文 -->
                 <h3 class="about_h3">なぜこのサイトを作ったのか</h3>
@@ -25,105 +29,114 @@
                     このサイトは、国の求職者支援制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
                 </p>
             </div>
+        </div>
 
         </div>
     </section>
 
-    <section class="about_container">
+    <section class="about_container shop_container">
         <!-- コンテンツボックス -->
         <div class="about_box flex_pc">
             <!-- テキスト用ボックス -->
             <div class="about_txtbox relative">
                 <!-- 見出し文 -->
-                <h3 class="about_h3">サイト制作に<br>
+                <h3 class="about_h3">サイト制作に
                     ご協力くださったみなさま</h3>
                 <!-- キャプション -->
                 <p class="about_txt ">この度は本サイト制作に際して、ご協力いただき誠にありがとうございます。</p>
                 <!-- ショップ一覧 -->
                 <div class="about_thanks_wrap">
-                    <table class="about_thanks_table">
-                        <tbody>
-                            <tr>
-                                <td>ベーカリーカフェ様</td>
+                    <TABLE class="about_table">
+                        <!-- １行目 -->
 
-                                <td>ベーカリーカフェ様</td>
-                            </tr>
-
-                            <tr>
-                                <td>ベーカリーカフェ様</td>
-
-                                <td>ベーカリーカフェ様</td>
-                            </tr>
-
-                            <tr>
-                                <td>カフェAAA様</td>
-
-                                <td>その他大勢の飲食店様</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                        <!-- ２行目 -->
+                        <TR>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                        </TR>
+                        <!-- ３行目 -->
+                        <TR>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                        </TR>
+                        <!-- １行目 -->
+                        <TR>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>その他ご協力くださったみなさま</TD>
+                        </TR>
+                    </TABLE>
                 </div>
 
-                <p>その他ご協力くださったみなさま</p>
+                <!-- <p class="about_txt ">その他ご協力くださったみなさま</p> -->
             </div>
 
         </div>
     </section>
 
-    <section class="about_container">
+    <section class="about_container shop_container">
         <!-- コンテンツボックス -->
         <div class="about_box flex_pc">
             <!-- テキスト用ボックス -->
             <div class="about_txtbox relative">
                 <!-- 見出し文 -->
-                <h3 class="about_h3">参考サイト</h3>
+                <h3 class="about_h3">サイト制作に
+                    ご協力くださったみなさま</h3>
                 <!-- キャプション -->
-                <p class="index_howto_txt ">
-                    以下のサイトを参考元・引用元として、まとめさせて頂きました。 各関係機関にご協力いただき、リンク先を掲載させていただいております。 アドレスをクリックすると、リンク先へ飛びます。
+                <p class="about_txt ">以下のサイトを参考元・引用元として、まとめさせて頂きました。 各関係機関にご協力いただき、リンク先を掲載させていただいております。 アドレスをクリックすると、リンク先へ飛びます。
                 </p>
-                <ul>
-                    <li>
-                        ベーカリーショップ
-                    </li>
-                    <li>
-                        ベーカリーショップ
-                    </li>
-                    <li>
-                        ベーカリーショップ
-                    </li>
-                    <li>
-                        ベーカリーショップ
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                    <li>
-                        カフェAAA
-                    </li>
-                </ul>
+                <!-- ショップ一覧 -->
+                <div class="about_thanks_wrap">
+                    <TABLE class="about_table">
+                        <!-- １行目 -->
+
+                        <!-- ２行目 -->
+                        <TR>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                            <TD>カフェAAA様</TD>
+                        </TR>
+                        <!-- ３行目 -->
+                        <TR>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                        </TR>
+                        <!-- １行目 -->
+                        <TR>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>ベーカリーカフェ様</TD>
+                            <TD>その他ご協力くださったみなさま</TD>
+                        </TR>
+                    </TABLE>
+                </div>
+
+                <!-- <p class="about_txt ">その他ご協力くださったみなさま</p> -->
             </div>
 
         </div>
     </section>
 </main>
+<!--------- フッター切り取りここから --------->
 
+<div class="news_bottom_container">
+    <!-- PCのみ出るニュース下部の自転車 -->
+    <img class="news_bottom_cycle_img" src="./assets/image/news_img.png" alt="ニュース下部装飾">
+
+    <!-- 下部背景の入れ物 -->
+    <div class="loop_wrap">
+        <!-- PCのみ出るニュース下部の背景 -->
+        <img src="./assets/image/news_bottom_bg_img.png"><img src="./assets/image/news_bottom_bg_img.png">
+    </div>
+</div>
+</main>
 
 <?php get_footer(); ?>
