8e2099b21792a7de282be081b381360e0b033f07
bando412 <sys5rb639@gmail.com>
2022-12-09 11:35:29 +0900

    下層キービジュ差し替え（坂東）

--- archive-spot.php
+++ archive-spot.php
@@ -4,7 +4,7 @@
 <main>
     <!-- キービジュアル -->
     <div class="key_bg_under">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/key_visual_orange.png" alt="マイページキービジュアル" class="key_bg_under_img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/key_visual_img4.png" alt="マイページキービジュアル" class="key_bg_under_img">
     </div>
 
     <!-- パンくずリスト -->
