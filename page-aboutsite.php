31fe113cd61e64906a38cdcd9e14dd0f32ac626d
bando412 <sys5rb639@gmail.com>
2022-12-11 13:23:07 +0900

    微調整、abousite追加（坂東）

--- page-aboutsite.php
+++ page-aboutsite.php
@@ -2,141 +2,168 @@
 <main>
     <!-- キービジュアル -->
     <div class="key_bg_under">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/key_visual_lightblue.png" alt="マイページキービジュアル" class="key_bg_under_img">
+        <img src="https://placehold.jp/1440x400.png" alt="マイページキービジュアル" class="key_bg_under_img">
     </div>
+
     <!-- ページ見出し -->
     <section class="aboutsite">
-        <h2 class="course_top headline"><ruby>ABOUT SITE<rt>このサイトについて</rt></ruby></h2>
-    </section>
-    <section class="about_container">
-        <!-- コンテンツボックス -->
-        <div class="about_box relative">
+        <h2 class="course_top headline"><ruby>ABOUT SITE<rt>サイト制作にあたって</rt></ruby></h2>
+
+        <div class="aboutsite_content_box">
             <!-- 最初のキャプション -->
-            <p class="about_txt">このサイトは、国の公的職業訓練制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
+            <p class="aboutsite_text">このサイトは、国の公的職業訓練制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
             </p>
-            <div class="flex_pc">
+
+            <!-- 写真とテキストのボックス -->
+            <div class="aboutsite_imgtext_box">
                 <!-- 写真 -->
-                <!-- <div class="index_howto_imgbox"> -->
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index_howto_img.png" alt="浜辺に自転車を置いて海に向かう男性" class="index_howto_img">
-            </div>
-            <!-- テキスト用ボックス -->
-            <div class="about_txtbox absolute">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/aboutsite_img1.png" alt="チームハッシュ集合写真" class="aboutsite_img">
 
-                <!-- 見出し文 -->
-                <h3 class="about_h3">なぜこのサイトを作ったのか</h3>
-                <!-- キャプション -->
-                <p class="about_txt">
-                    このサイトは、国の求職者支援制度に基づく、株式会社ＱＬＩＰ インターナショナル・ＷＥＢシステム開発実践科生『井（ハッシュ）』班が、2022年に制作したウェブサイトです。（以下「本サイト」と言います）
-                </p>
-            </div>
-        </div>
-
-        </div>
-    </section>
-
-    <section class="about_container shop_container">
-        <!-- コンテンツボックス -->
-        <div class="about_box flex_pc">
-            <!-- テキスト用ボックス -->
-            <div class="about_txtbox relative">
-                <!-- 見出し文 -->
-                <h3 class="about_h3">サイト制作に
-                    ご協力くださったみなさま</h3>
-                <!-- キャプション -->
-                <p class="about_txt ">この度は本サイト制作に際して、ご協力いただき誠にありがとうございます。</p>
-                <!-- ショップ一覧 -->
-                <div class="about_thanks_wrap">
-                    <TABLE class="about_table">
-                        <!-- １行目 -->
-
-                        <!-- ２行目 -->
-                        <TR>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                        </TR>
-                        <!-- ３行目 -->
-                        <TR>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                        </TR>
-                        <!-- １行目 -->
-                        <TR>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>その他ご協力くださったみなさま</TD>
-                        </TR>
-                    </TABLE>
+                <div class="aboutsite_text_box">
+                    <!-- 見出し文 -->
+                    <h3 class="aboutsite_h3">サイト制作の経緯</h3>
+                    <!-- キャプション -->
+                    <p class="aboutsite_text">
+                        2022年10月、徳島県と兵庫県淡路島を結ぶ大鳴門橋に自転車道を整備するという方針が示されました。鳴門市は徳島県において関西の玄関口であり、サイクリング愛好家に人気の淡路島から人を呼び込むことが狙いです。<br>
+                        本サイトでは、鳴門のサイクリングコースやその周辺の魅力的なスポットを紹介しています。もともと鳴門は
+                        これからますます盛り上がりを見せる鳴門市の振興に貢献できればと考えています。
+                    </p>
                 </div>
-
-                <!-- <p class="about_txt ">その他ご協力くださったみなさま</p> -->
             </div>
+        </div>
+
+        <div class="about_thanks_box">
+            <!-- 見出し文 -->
+            <h3 class="aboutsite_h3">サイト制作にご協力くださった皆様</h3>
+            <!-- キャプション -->
+            <p class="aboutsite_text ">この度は本サイト制作に際して、ご協力いただき誠にありがとうございます。</p>
+
+            <h4 class="aboutsite_h4">インタビュー</h4>
+            <!-- スポット一覧 -->
+            <ul class="thanks_list">
+                <li>
+                    <a href="http://cycling.co.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">ナカニシサイクル 中西様</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://cycle-uzu.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">CYCLE SPACE UZU 赤谷様</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.city.naruto.tokushima.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">鳴門市役所 佐藤様</div>
+                    </a>
+                </li>
+
+            </ul>
+
+            <h4 class="aboutsite_h4">掲載許可</h4>
+            <!-- スポット一覧 -->
+            <ul class="thanks_list">
+                <li>
+                    <a href="http://cycling.co.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">ナカニシサイクル</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://cycle-uzu.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">CYCLE SPACE UZU</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://binbi-ya.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">活魚料理 びんび家</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.e-sampa.net" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">支那そば三八 黒崎店</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://o-museum.or.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">大塚国際美術館</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://doitsukan.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">鳴門市ドイツ館
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.city.naruto.tokushima.jp/contents/michino_eki/index.html" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">道の駅 第九の里</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://narutokaigetsu.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">ベイリゾートホテル鳴門海月</div>
+                    </a>
+                </li>
+            </ul>
+            <!-- 最後の項目 -->
+            <p class="thanks_list_all">ご協力くださったすべての皆様</p>
+
+            <!-- 見出し文 -->
+            <h3 class="aboutsite_h3">参考サイト</h3>
+            <!-- キャプション -->
+            <p class="aboutsite_text">以下のサイトを参考元・引用元として、まとめさせて頂きました。<br> 各関係機関にご協力いただき、リンク先を掲載させていただいております。 アドレスをクリックすると、リンク先へ飛びます。</p>
+
+            <!-- 参考 -->
+            <h4 class="aboutsite_h4">データ</h4>
+            <!-- スポット一覧 -->
+            <ul class="thanks_list">
+                <li>
+                    <a href="https://asa-cycling.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">ASAサイクリングコース</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.bestbike.jp/?contents=shop&dir=1002" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">SR鳴門市レンタサイクル</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.google.com/maps/place/%E5%BE%B3%E5%B3%B6%E7%9C%8C%E9%B3%B4%E9%96%80%E5%B8%82/@34.1914407,134.4732608,12z/data=!3m1!4b1!4m5!3m4!1s0x35537004b8db8063:0xbfd0dc7e9484a6ae!8m2!3d34.1726034!4d134.608866
+" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">Googleマップ</div>
+                    </a>
+                </li>
+            </ul>
+
+            <!-- 画像・イラスト -->
+            <h4 class="aboutsite_h4">画像・イラスト</h4>
+            <!-- スポット一覧 -->
+            <ul class="thanks_list">
+                <li>
+                    <a href="https://www.tokusupo.net/bicycle/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">すだちくんアイコン</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.skr.mlit.go.jp/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">道の駅シンボルマーク</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.photo-ac.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">photoAC</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.ac-illust.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="thanks_spot">illustAC</div>
+                    </a>
+                </li>
+            </ul>
 
         </div>
+
+
     </section>
-
-    <section class="about_container shop_container">
-        <!-- コンテンツボックス -->
-        <div class="about_box flex_pc">
-            <!-- テキスト用ボックス -->
-            <div class="about_txtbox relative">
-                <!-- 見出し文 -->
-                <h3 class="about_h3">サイト制作に
-                    ご協力くださったみなさま</h3>
-                <!-- キャプション -->
-                <p class="about_txt ">以下のサイトを参考元・引用元として、まとめさせて頂きました。 各関係機関にご協力いただき、リンク先を掲載させていただいております。 アドレスをクリックすると、リンク先へ飛びます。
-                </p>
-                <!-- ショップ一覧 -->
-                <div class="about_thanks_wrap">
-                    <TABLE class="about_table">
-                        <!-- １行目 -->
-
-                        <!-- ２行目 -->
-                        <TR>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                            <TD>カフェAAA様</TD>
-                        </TR>
-                        <!-- ３行目 -->
-                        <TR>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                        </TR>
-                        <!-- １行目 -->
-                        <TR>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>ベーカリーカフェ様</TD>
-                            <TD>その他ご協力くださったみなさま</TD>
-                        </TR>
-                    </TABLE>
-                </div>
-
-                <!-- <p class="about_txt ">その他ご協力くださったみなさま</p> -->
-            </div>
-
-        </div>
-    </section>
-</main>
-<!--------- フッター切り取りここから --------->
-
-<div class="news_bottom_container">
-    <!-- PCのみ出るニュース下部の自転車 -->
-    <img class="news_bottom_cycle_img" src="./assets/image/news_img.png" alt="ニュース下部装飾">
-
-    <!-- 下部背景の入れ物 -->
-    <div class="loop_wrap">
-        <!-- PCのみ出るニュース下部の背景 -->
-        <img src="./assets/image/news_bottom_bg_img.png"><img src="./assets/image/news_bottom_bg_img.png">
-    </div>
-</div>
 </main>
 
 <?php get_footer(); ?>
