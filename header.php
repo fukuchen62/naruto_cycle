9e66829c98f965e0da4c3ac472972fb23ce0c20c
tetsu <tetsunobu610@gmail.com>
2023-01-14 18:02:00 +0900

    googlefonts読み込み修正
    preconnectを直接読み込みに修正
    
    佐藤

--- header.php
+++ header.php
@@ -8,7 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="format-detection" content="telephone=no" />
-
+    <!-- googlefont本体ここから2行 -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <?php
+    //  wp_deregister_script('jquery');
+    ?>
     <?php wp_head(); ?>
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/head_favicon.ico">
 </head>
